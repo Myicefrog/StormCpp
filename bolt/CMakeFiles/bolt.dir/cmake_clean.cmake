59e41bc962b09ea78f9054a8d769f6bdad2dae40
Myicefrog <490925498@qq.com>
2018-03-18 23:22:02 -0700

    cpp wrap storm both bolt and spout

---
+++ bolt/CMakeFiles/bolt.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bolt.dir/home/luguang/demo/StormCpp/public/Storm.cpp.o"
+  "CMakeFiles/bolt.dir/SplitSentenceTest.cpp.o"
+  "bolt.pdb"
+  "bolt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bolt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ spout/CMakeFiles/spout.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spout.dir/home/luguang/demo/StormCpp/public/Storm.cpp.o"
+  "CMakeFiles/spout.dir/SplitSentenceSpoutTest.cpp.o"
+  "spout.pdb"
+  "spout"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spout.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
